8cb50a69589bfdac1838bdc5e9446881c0458dae
Joe Bailey <baileyrva@gmail.com>
2020-03-21 15:12:42 -0400

    mongodb username

--- server.js
+++ server.js
@@ -14,8 +14,11 @@ app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
 app.use(express.static("public"));
-var MONGODB_URI = process.env.MONGODB_URI || "mongodb://localhost/budget";
-mongoose.connect(MONGODB_URI); 
+
+mongoose.connect("mongodb://localhost/budget", {
+  useNewUrlParser: true,
+  useFindAndModify: false
+});
 
 // routes
 app.use(require("./routes/api.js"));
